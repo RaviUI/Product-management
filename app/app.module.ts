97d5889ba29c679fc96e4605c1d0c1374422477b
Raviui <you@example.com>
2017-06-05 11:30:35 -0400

    Add existing files

---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { StarComponent } from './shared/star.component';
+
+import { AppComponent }  from './app.component';
+import {ProductListComponent} from './products/product-list.component';
+import {ProductFilterPipe} from './products/product-filter.pipe';
+
+@NgModule({
+  imports: [ BrowserModule, FormsModule],
+  declarations: [ AppComponent, ProductListComponent, ProductFilterPipe, StarComponent],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
